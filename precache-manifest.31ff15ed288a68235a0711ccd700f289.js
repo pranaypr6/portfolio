feee8f2c6415f10a0c2b65deac5aef2b3b9d8b1d
unknown <pranayburra66@gmail.com>
2020-07-05 01:34:59 +0530

    Updates

---
+++ precache-manifest.31ff15ed288a68235a0711ccd700f289.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891f09ad369b84924729e790150214fe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "338bfee3402fd33cf116",
+    "url": "/portfolio/static/css/main.d4e68d03.chunk.css"
+  },
+  {
+    "revision": "5a6e7965d3e772623a31",
+    "url": "/portfolio/static/js/2.e4d897f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.e4d897f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338bfee3402fd33cf116",
+    "url": "/portfolio/static/js/main.b45ab792.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "46d9f22baccdf7718ee608f0b5503c6c",
+    "url": "/portfolio/static/media/coding_2.46d9f22b.svg"
+  }
+]);
